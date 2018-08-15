b42ec3fb2191c88a35065b6d16cc753d0e391aa3
Graham Davis <davis.graham.satx@gmail.com>
2018-08-15 09:12:29 -0500

    completed groub by exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,44 @@
+use employees;
+
+# use DISTINCT to find the unique titles in the titles table
+SELECT DISTINCT title
+FROM titles;
+
+
+# Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY.
+select last_name
+from employees
+where last_name like 'e%'
+and last_name like '%e'
+group by last_name;
+
+
+
+# Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+select last_name, first_name
+from employees
+where last_name like 'e%' and last_name like '%e'
+order by last_name, first_name;
+
+
+# Find the unique last names with a 'q' but not 'qu'. Your results should be:
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+
+
+# Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT first_name, last_name, COUNT(*)
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+GROUP BY last_name, first_name
+ORDER BY last_name;
+
+
+# Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
--- group_by_lecture.sql
+++ group_by_lecture.sql
@@ -15,3 +15,25 @@ select count(birth_date)
 from employees
 where birth_date = '1961-05-03'
 group by birth_date;
+
+
+select birth_date, count(birth_date) as 'number of birthday\'s on the selected day'
+from employees
+where birth_date = '1961-05-03'
+  or birth_date = '1958-05-03'
+group by birth_date;
+
+select *
+from employees
+where birth_date = '1958-05-03';
+# where birth_date = '1961-05-03';
+
+select count(*) as 'counted birthdays'
+from employees
+where birth_date = '1961-05-03'
+or birth_date = '1958-05-03';
+
+
+select count(emp_no)
+from employees
+where first_name = 'Kokou'  and gender = 'm';
